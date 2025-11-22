e38e4a83270bbe4c9eff84aa1e4ac3e7688e393f
Maab3li <maab3li555@gmail.com>
2025-11-22 14:45:32 -0800

    fix:changed theme toggler src to the exact path on gh pages to fetch theme correctly on host

--- js/main.js
+++ js/main.js
@@ -231,7 +231,7 @@ console.log(document.getElementById('theme-toggler').src)
 
 //function to toggle themes
 function toggleTheme() {
-    if(document.getElementById('theme-toggler').src == './assets/images/icon-sun.svg') {
+    if(document.getElementById('theme-toggler').src == 'https://github.com/Maab3li/browser-extensions-manager-ui-main/blob/master/assets/images/icon-sun.svg') {
         document.getElementById('logo').src = './assets/images/logo.svg'
         document.getElementById('theme-toggler').src = './assets/images/icon-moon.svg'
         document.querySelector('html').setAttribute('data-theme', 'light')
